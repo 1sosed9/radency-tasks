b05c0d4a87ff528f625d8ad8ad115b46bf3b3f9e
1sosed9 <vladsosed63@gmail.com>
2023-07-29 00:30:30 +0300

    finished project without data

---
+++ src/routes/notes.route.ts
@@ -0,0 +1,19 @@
+// src/routes/notes.route.ts
+import { Router } from 'express';
+import {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  updateNote,
+  deleteNote,
+} from '../controllers/notes.controller';
+
+const router = Router();
+
+router.get('/notes', getAllNotes);
+router.get('/notes/:id', getNoteById);
+router.post('/notes', createNote);
+router.patch('/notes/:id', updateNote);
+router.delete('/notes/:id', deleteNote);
+
+export default router;
